b0623dc0900cda51f6b2bfa2fc02080920bfe867
Adam Smith <asmith.bassoon@gmail.com>
2017-09-11 20:13:28 -0400

    updated connection string

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -1,63 +1,69 @@
 module.exports = function(app)
 {
 
-    app.get('/api', (req, res) => {
-      res.send('App works');
-    });
-    app.get("/api/test", findAllMessages);
-    app.post("/api/test", createMessage);
-    app.delete("/api/test/:id", deleteMessage);
+  app.get('/api', (req, res) => {
+    res.send('App works');
+  });
+  app.get("/api/test", findAllMessages);
+  app.post("/api/test", createMessage);
+  app.delete("/api/test/:id", deleteMessage);
 
 
-  var connectionString = 'mongodb://webappmaker:webappmaker@ds163181.mlab.com:63181/webappmaker';
+  var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+  if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.evn.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ":" + password;
+    connectionString += '@ds133084.mlab.com:33084/heroku_kds2x3lm'; // use yours
+  }
 
   var mongoose = require("mongoose");
-    mongoose.connect(connectionString);
+  mongoose.connect(connectionString);
 
-    var TestSchema = mongoose.Schema({
-        message: String
-    });
+  var TestSchema = mongoose.Schema({
+    message: String
+  });
 
-    var TestModel = mongoose.model("TestModel", TestSchema);
+  var TestModel = mongoose.model("TestModel", TestSchema);
 
-    function findAllMessages(req, res) {
+  function findAllMessages(req, res) {
 
-        TestModel
-            .find()
-            .then(
-                function(tests) {
-                    res.json(tests);
-                },
-                function(err) {
-                    res.status(400).send(err);
-                }
-            );
-    }
+    TestModel
+      .find()
+      .then(
+        function(tests) {
+          res.json(tests);
+        },
+        function(err) {
+          res.status(400).send(err);
+        }
+      );
+  }
 
-    function createMessage(req, res) {
-        console.log("in app");
-        TestModel
-            .create(req.body)
-            .then(
-                function(test) {
-                    res.json(test);
-                },
-                function(err) {
-                    res.status(400).send(err);
-                }
-            );
-    }
+  function createMessage(req, res) {
+    console.log("in app");
+    TestModel
+      .create(req.body)
+      .then(
+        function(test) {
+          res.json(test);
+        },
+        function(err) {
+          res.status(400).send(err);
+        }
+      );
+  }
 
-    function deleteMessage(req, res) {
-        TestModel
-            .remove({_id: req.params.id})
-            .then(
-                function(result) {
-                    res.json(result);
-                },
-                function(err) {
-                    res.status(400).send(err);
-                }
-            );
-    }
+  function deleteMessage(req, res) {
+    TestModel
+      .remove({_id: req.params.id})
+      .then(
+        function(result) {
+          res.json(result);
+        },
+        function(err) {
+          res.status(400).send(err);
+        }
+      );
+  }
 };
