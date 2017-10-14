7c76f13a179bfb1263af947406e6b9af4841eed5
Adam Smith <asmith.bassoon@gmail.com>
2017-10-14 17:37:19 -0400

    created and implemented user.service.client.ts

---
+++ src/app/services/user.service.client.ts
@@ -0,0 +1,60 @@
+import { Injectable} from '@angular/core';
+
+@Injectable()
+export class UserService {
+
+  constructor() {}
+  users = [
+    {_id: '123', username: 'alice',    password: 'alice',    firstName: 'Alice',  lastName: 'Wonder'  },
+    {_id: '234', username: 'bob',      password: 'bob',      firstName: 'Bob',    lastName: 'Marley'  },
+    {_id: '345', username: 'charly',   password: 'charly',   firstName: 'Charly', lastName: 'Garcia'  },
+    {_id: '456', username: 'jannunzi', password: 'jannunzi', firstName: 'Jose',   lastName: 'Annunzi' }
+  ];
+  api = {
+    'createUser' : this.createUser,
+    'findUserById' : this.findUserById,
+    'findUserByUsername' : this.findUserByUsername,
+    'findUserByCredentials' : this.findUserByCredentials,
+    'updateUser' : this.updateUser,
+    'deleteUser' : this.deleteUser
+  };
+  createUser(user: any) {
+    user._id = Math.random();
+    this.users.push(user);
+    return user;
+  }
+  findUserById(userId: String) {
+    for (let x = 0; x < this.users.length; x++) {
+      if (this.users[x]._id === userId) {
+        return this.users[x];
+      }
+    }
+  }
+  findUserByUsername(username: String) {
+    return this.users.find(function (user) {
+      return user.username === username;
+    });
+  }
+  findUserByCredentials(username: String, password: String) {
+    return this.users.find(function(user) {
+      return user.username === username && user.password === password;
+    });
+  }
+  updateUser(userId, user) {
+    for (let x = 0; x < this.users.length; x++) {
+      const _user = this.users[x];
+      if (_user._id === userId) {
+        this.users[x].firstName = user.firstName;
+        this.users[x].lastName = user.lastName;
+        this.users[x].password = user.password;
+      }
+    }
+  }
+  deleteUser(userId) {
+    for (let x = 0; x < this.users.length; x++) {
+      if (this.users[x]._id === userId) {
+        this.users.splice(x, 1);
+      }
+    }
+  }
+}
