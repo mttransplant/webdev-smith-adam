037eb0770cb840bba6295d91adaae0ba96e375df
Adam Smith <asmith.bassoon@gmail.com>
2017-10-14 16:51:56 -0400

    refactored links in the user and page components.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,18 +6,18 @@ import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {ModuleWithProviders} from '@angular/core';
 import {TestComponent} from './components/test/test.component';
-import {LoginComponent} from "./components/user/login/login.component";
-import {RegisterComponent} from "./components/user/register/register.component";
-import {ProfileComponent} from "./components/user/profile/profile.component";
-import {WebsiteListComponent} from "./components/website/website-list/website-list.component";
-import {WebsiteNewComponent} from "./components/website/website-new/website-new.component";
-import {WebsiteEditComponent} from "./components/website/website-edit/website-edit.component";
-import {PageListComponent} from "./components/page/page-list/page-list.component";
-import {PageNewComponent} from "./components/page/page-new/page-new.component";
-import {PageEditComponent} from "./components/page/page-edit/page-edit.component";
-import {WidgetListComponent} from "./components/widget/widget-list/widget-list.component";
-import {WidgetChooserComponent} from "./components/widget/widget-chooser/widget-chooser.component";
-import {WidgetEditComponent} from "./components/widget/widget-edit/widget-edit.component";
+import {LoginComponent} from './components/user/login/login.component';
+import {RegisterComponent} from './components/user/register/register.component';
+import {ProfileComponent} from './components/user/profile/profile.component';
+import {WebsiteListComponent} from './components/website/website-list/website-list.component';
+import {WebsiteNewComponent} from './components/website/website-new/website-new.component';
+import {WebsiteEditComponent} from './components/website/website-edit/website-edit.component';
+import {PageListComponent} from './components/page/page-list/page-list.component';
+import {PageNewComponent} from './components/page/page-new/page-new.component';
+import {PageEditComponent} from './components/page/page-edit/page-edit.component';
+import {WidgetListComponent} from './components/widget/widget-list/widget-list.component';
+import {WidgetChooserComponent} from './components/widget/widget-chooser/widget-chooser.component';
+import {WidgetEditComponent} from './components/widget/widget-edit/widget-edit.component';
 
 
 
