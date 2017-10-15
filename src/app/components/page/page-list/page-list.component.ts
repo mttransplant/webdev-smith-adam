6bf420579188eee7c1ccedf7bf20ee0f2641fa5c
Adam Smith <asmith.bassoon@gmail.com>
2017-10-15 18:24:49 -0400

    Populate page list page

--- src/app/components/page/page-list/page-list.component.ts
+++ src/app/components/page/page-list/page-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {PageService} from '../../../services/page.service.client';
+import {ActivatedRoute} from '@angular/router';
+import {WebsiteService} from '../../../services/website.service.client';
 
 @Component({
   selector: 'app-page-list',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageListComponent implements OnInit {
 
-  constructor() { }
+  // parameters
+  userId: String;
+  websiteId: String;
+  pages = [{_id: '', name: '', websiteId: '', description: ''}];
+
+  constructor(private _pageService: PageService, private activatedRoute: ActivatedRoute, private _websiteService: WebsiteService) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.websiteId = params['websiteId'];
+        }
+      );
+    this.pages = this._pageService.findPagesByWebsiteId(this.websiteId);
+    this.userId = this._websiteService.findWebsiteById(this.websiteId).developerId;
   }
 
 }
