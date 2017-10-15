e48bd5dc3f9666b8bdaef0869fbfc14dfbe2cf0d
Adam Smith <asmith.bassoon@gmail.com>
2017-10-15 18:40:43 -0400

    populate widget list page

--- src/app/components/widget/widget-list/widget-list.component.ts
+++ src/app/components/widget/widget-list/widget-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {PageService} from '../../../services/page.service.client';
+import {ActivatedRoute} from '@angular/router';
+import {WebsiteService} from '../../../services/website.service.client';
+import {WidgetService} from '../../../services/widget.service.client';
 
 @Component({
   selector: 'app-widget-list',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WidgetListComponent implements OnInit {
 
-  constructor() { }
+  // parameters
+  userId: String;
+  websiteId: String;
+  pageId: String;
+  widgets = [{_id: '', widgetType: '', pageId: '', size: 0, text: '', width: '', url: ''}];
+
+  constructor(private _widgetService: WidgetService,
+              private _pageService: PageService,
+              private activatedRoute: ActivatedRoute,
+              private _websiteService: WebsiteService) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.pageId = params['pageId'];
+        }
+      );
+    this.widgets = this._widgetService.findWidgetByPageId(this.pageId);
+    this.websiteId = this._pageService.findPageById(this.pageId).websiteId;
+    this.userId = this._websiteService.findWebsiteById(this.websiteId).developerId;
   }
 
 }
