e1434a7b0cefb84ef6f713069d7fcc936bd6d488
Adam Smith <asmith.bassoon@gmail.com>
2017-10-15 16:51:22 -0400

    updated populate website form

--- src/app/components/website/website-edit/website-edit.component.ts
+++ src/app/components/website/website-edit/website-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import {WebsiteService} from '../../../services/website.service.client';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-website-edit',
@@ -10,16 +12,26 @@ export class WebsiteEditComponent implements OnInit {
   @ViewChild('websiteEditForm') websiteEditForm: NgForm;
 
   // properties
+  websiteId: String;
+  website = {_id: '', name: '', developerId: '', description: ''};
   name: String;
   description: String;
 
-  constructor() { }
+  constructor(private websiteService: WebsiteService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.websiteId = params['websiteId'];
+        }
+      );
+    this.website = this.websiteService.findWebsiteById(this.websiteId);
+    this.name = this.website['name'];
   }
 
   editWebsite() {
-    this.name = this.websiteEditForm.value.name;
-    this.description = this.websiteEditForm.value.description;
+    this.website.name = this.websiteEditForm.value.name;
+    this.website.description = this.websiteEditForm.value.description;
   }
 }
