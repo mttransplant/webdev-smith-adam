1205ba1622cc4d471a25b470809f9e54cdd0f314
Adam Smith <asmith.bassoon@gmail.com>
2017-10-14 20:09:43 -0400

    refactoring login forms

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+  @ViewChild('f') loginForm: NgForm;
+
+  // properties
+  username: String;
+  password: String;
+  errorFlag: Boolean = false;
+  errorMsg = 'Invalid username or password!';
 
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  login () {
+    this.username = this.loginForm.value.username;
+    this.password = this.loginForm.value.password;
   }
 
 }
