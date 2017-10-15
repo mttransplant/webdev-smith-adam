19dd80598fbe0a0e01bec47f34dcd28f85311223
Adam Smith <asmith.bassoon@gmail.com>
2017-10-15 16:44:06 -0400

    updated populate page form elements

--- src/app/components/page/page-edit/page-edit.component.ts
+++ src/app/components/page/page-edit/page-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import {PageService} from '../../../services/page.service.client';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-page-edit',
@@ -10,16 +12,26 @@ export class PageEditComponent implements OnInit {
   @ViewChild('pageEditForm') pageEditForm: NgForm;
 
   // properties
+  pageId: String;
+  page = {_id: '', name: '', websiteId: '', description: ''};
   name: String;
   title: String;
 
-  constructor() { }
+  constructor(private pageService: PageService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.pageId = params['pageId'];
+        }
+      );
+    this.page = this.pageService.findPageById(this.pageId);
+    this.name = this.page['name'];
   }
 
   editPage() {
-    this.name = this.pageEditForm.value.name;
-    this.title = this.pageEditForm.value.title;
+    this.page.name = this.pageEditForm.value.name;
+    this.page.description = this.pageEditForm.value.title;
   }
 }
--- src/app/services/page.service.client.ts
+++ src/app/services/page.service.client.ts
@@ -9,9 +9,9 @@ export class PageService {
 
   constructor() {}
   pages = [
-    { '_id': '321', 'name': 'Post 1', 'websiteId': '456', 'description': 'Lorem' },
-    { '_id': '432', 'name': 'Post 2', 'websiteId': '456', 'description': 'Lorem' },
-    { '_id': '543', 'name': 'Post 3', 'websiteId': '456', 'description': 'Lorem' }
+    { _id: '321', name: 'Post 1', websiteId: '456', description: 'Lorem' },
+    { _id: '432', name: 'Post 2', websiteId: '456', description: 'Lorem' },
+    { _id: '543', name: 'Post 3', websiteId: '456', description: 'Lorem' }
   ];
   api = {
     'createPage' : this.createPage,
