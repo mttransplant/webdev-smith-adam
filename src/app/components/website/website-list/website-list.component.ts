bd575878b01930bd4f2db0b77139857a219d5f0a
Adam Smith <asmith.bassoon@gmail.com>
2017-10-15 17:56:40 -0400

    populate website list page

--- src/app/components/website/website-list/website-list.component.ts
+++ src/app/components/website/website-list/website-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {WebsiteService} from '../../../services/website.service.client';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-website-list',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebsiteListComponent implements OnInit {
 
-  constructor() { }
+  // parameters
+  userId: String;
+  websites = [{_id: '', name: '', developerId: '', description: ''}];
+
+  constructor(private _websiteService: WebsiteService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.userId = params['userId'];
+        }
+      );
+    this.websites = this._websiteService.findWebsitesByUser(this.userId);
   }
 
 }
