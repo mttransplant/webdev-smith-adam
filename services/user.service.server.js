1c131740c26f584b2ff7a99921e9cd779af30029
Adam Smith <asmith.bassoon@gmail.com>
2017-10-26 18:07:33 -0400

    Create Web Services

--- services/user.service.server.js
+++ services/user.service.server.js
@@ -1,7 +1,8 @@
 module.exports = function(app) {
-  // app.post('/api/user/:userId/website', createWebsite);
-  // app.get('/api/user/:userId/website', findAllWebsitesForUser);
-  // app.get('/api/website/:websiteId', findWebsiteById);
-  // app.put('/api/website/:websiteId', updateWebsite);
-  // app.delete('/api/website/:websiteId', deleteWebsite);
+  // app.post('/api/user/', createUser);
+  // app.get('/api/user/?username=username', finUserByUsername);
+  // app.get('/api/user?username=username&password=password', findUserByCredentials);
+  // app.get('/api/user/:userId', findUserById);
+  // app.put('/api/user/:userId', updateUser);
+  // app.delete('/api/user/:userId', deleteUser);
 }
